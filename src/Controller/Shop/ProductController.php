b465f19ca1af81b61b7676c9607c6093fd7c2774
dimaldim <antraks92@gmail.com>
2019-07-11 16:19:12 +0300

    Init Product controller

--- src/Controller/Shop/ProductController.php
+++ src/Controller/Shop/ProductController.php
@@ -2,20 +2,21 @@
 
 namespace App\Controller\Shop;
 
+use App\Entity\Products;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TestController extends AbstractController
+class ProductController extends AbstractController
 {
     /**
-     * @Route("/test", name="test")
+     * @Route("/product/{slug}", name="view_product")
      */
-    public function index()
+    public function viewProduct(Products $product)
     {
         return $this->render(
-            'shop/test/index.html.twig',
+            'shop/product/view.html.twig',
             [
-                'controller_name' => 'TestController',
+
             ]
         );
     }
