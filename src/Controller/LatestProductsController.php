7ba4a5b65e904d55424b099e6b1fe0708883c942
dimaldim <antraks92@gmail.com>
2019-07-11 11:22:58 +0300

    Add controller for 'Latest products'

---
+++ src/Controller/LatestProductsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Products;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LatestProductsController extends AbstractController
+{
+    public function getLatestProducts($limit = 10)
+    {
+        $repository = $this->getDoctrine()->getRepository(Products::class);
+        $latestProducts = $repository->findBy([], ['dateAdded' => 'desc'], $limit);
+
+        return $this->render(
+            'latest_products.html.twig',
+            [
+                'products' => $latestProducts,
+            ]
+        );
+    }
+}
