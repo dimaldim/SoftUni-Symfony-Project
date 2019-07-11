2846ef6f36fe67cfbfd84ed6be1cd2469b3976b1
dimaldim <antraks92@gmail.com>
2019-07-11 16:50:40 +0300

    [Update] Redirect /login to / if user is logged in.

--- src/Controller/Shop/AuthController.php
+++ src/Controller/Shop/AuthController.php
@@ -14,9 +14,10 @@ class AuthController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //    $this->redirectToRoute('target_path');
-        // }
+        //redirect if user is logged in
+        if ($this->getUser()) {
+            return $this->redirectToRoute('homepage');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
