08342726097a871ce15339add6b349378085e679
dimaldim <antraks92@gmail.com>
2019-07-16 16:55:02 +0300

    Update CartService

--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -22,4 +22,23 @@ class CartService
         }
         $session->set('shopping_cart', $cart);
     }
+
+    public function getCartHeaderInfo()
+    {
+        $session = new Session();
+        $cart = $session->get('shopping_cart');
+        $totalItems = 0;
+        $totalPrice = 0.00;
+        if ($cart) {
+            foreach ($cart as $key => $value) {
+                $totalItems += $cart[$key]['qty'];
+                $totalPrice += $cart[$key]['price'] * $cart[$key]['qty'];
+            }
+        }
+
+        return [
+            'totalItems' => $totalItems,
+            'totalPrice' => $totalPrice,
+        ];
+    }
 }
