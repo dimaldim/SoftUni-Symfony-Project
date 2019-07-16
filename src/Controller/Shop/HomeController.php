3a6113f7a256e9b9a90837d546f99be43753b01f
dimaldim <antraks92@gmail.com>
2019-07-16 11:56:00 +0300

    Change HomeController

--- src/Controller/Shop/HomeController.php
+++ src/Controller/Shop/HomeController.php
@@ -4,12 +4,15 @@ namespace App\Controller\Shop;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     * @return Response
      */
     public function index(Request $request)
     {
