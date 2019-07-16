e0f85ec130fe0ae9ef32be384ed45909b837c3ab
dimaldim <antraks92@gmail.com>
2019-07-16 14:15:02 +0300

    Update CartController

--- src/Controller/Shop/CartController.php
+++ src/Controller/Shop/CartController.php
@@ -12,11 +12,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart/add/{id}", name="cart_add_product")
+     * @Route("/cart/add/{id}/{qty}", name="cart_add_product")
      * @param Products $product
+     * @param $qty
      * @return Response
      */
-    public function addToCart(Products $product = null)
+    public function addToCart(Products $product = null, $qty)
     {
         if ($product == null) {
             return new JsonResponse(
@@ -28,9 +29,13 @@ class CartController extends AbstractController
         $session = new Session();
         $cart = $session->get('shopping_cart');
         if (isset($cart[$product->getId()])) {
-            $cart[$product->getId()]['qty']++;
+            $cart[$product->getId()]['qty'] += $qty;
         } else {
-            $cart[$product->getId()] = ['price' => $product->getPrice(), 'qty' => 1];
+            $cart[$product->getId()] = [
+                'productName' => $product->getName(),
+                'price' => $product->getPrice(),
+                'qty' => $qty,
+            ];
         }
         $session->set('shopping_cart', $cart);
 
@@ -38,6 +43,28 @@ class CartController extends AbstractController
         return new JsonResponse(
             [
                 'data' => $cart = $session->get('shopping_cart'),
+            ]
+        );
+    }
+
+    public function getHeaderCartInfo()
+    {
+        $session = new Session();
+        $cart = $session->get('shopping_cart');
+        $totalItems = 0;
+        $totalPrice = 0.00;
+        if ($cart) {
+            foreach ($cart as $key => $value) {
+                $totalItems += $cart[$key]['qty'];
+                $totalPrice += $cart[$key]['price'] * $cart[$key]['qty'];
+            }
+        }
+
+        return $this->render(
+            'shop/cart.html.twig',
+            [
+                'totalItems' => $totalItems,
+                'totalPrice' => $totalPrice,
             ]
         );
     }
