03ac2f69618572956a03c1050224457edf877f78
dimaldim <antraks92@gmail.com>
2019-07-19 12:06:31 +0300

    Update(change design, add webpack encore  with vue loader...)

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you require will output into a single css file (app.css in this case)
+const $ = require('jquery');
+require('bootstrap');
+require('bootstrap/dist/css/bootstrap.min.css');
+require('../js/bootstrap-notify.min.js');
+require('../js/slick.min.js');
+require('../js/nouislider.min.js');
+require('../js/main.js');
+
+require('../css/style.css');
+require('../css/slick.css');
+require('../css/slick-theme.css');
+require('../css/nouislider.min.css');
+require('../css/font-awesome.min.css');
